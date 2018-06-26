d03a99170333e9741cdac0ceb33439910dd58421
Jessica Shu <jessicashu7@fb.com>
2018-06-26 13:27:00 -0700

    Initial Commit

---
+++ Tipster/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tipster
+//
+//  Created by Jessica Shu on 6/26/18.
+//  Copyright © 2018 Jessica Shu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Tipster/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Tipster
+//
+//  Created by Jessica Shu on 6/26/18.
+//  Copyright © 2018 Jessica Shu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
